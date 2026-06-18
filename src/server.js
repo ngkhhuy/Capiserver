e44ae1064668e3b88bd3aab5e9cab18c16e164a8
ngkhhuy <khanhhuychc@gmail.com>
2026-06-18 17:06:27 +0700

    update 3

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ if (REQUIRE_SERVER_KEY && !SERVER_KEY) {
   );
 }
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(express.json({ limit: '50kb' }));
 app.use(express.urlencoded({ extended: true, limit: '50kb' }));
